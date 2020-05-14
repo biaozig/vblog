3213280df77f51696446a5dc4ea4ba1d1bfefb8e
张琛琛 <zhangchenchen@qizhuyun.com>
2020-05-14 18:52:56 +0800

    关注

---
+++ src/pages/Workbench/Attention/Attention.tsx
@@ -0,0 +1,18 @@
+import React, { } from 'react';
+import { Table } from 'antd'
+
+import './Attention.scss';
+
+// 发布
+function Attention () {
+
+    return (
+        <div className='app-workbench-attention'>
+            
+        </div>
+    )
+
+
+}
+
+export default Attention;

a097879942b6fa5be001765d63c2b7b0aa79e957
张琛琛 <zhangchenchen@qizhuyun.com>
2020-05-14 18:51:00 +0800

    收藏

---
+++ src/pages/Workbench/Collection/Collection.tsx
@@ -0,0 +1,18 @@
+import React, { } from 'react';
+import { Table } from 'antd'
+
+import './Collection.scss';
+
+// 收藏
+function Collection () {
+
+    return (
+        <div className='app-workbench-collection'>
+            ddddddddddddddddddddddd
+        </div>
+    )
+
+
+}
+
+export default Collection;

58b90018032568d54be10cdd62612774ebad0374
张琛琛 <zhangchenchen@qizhuyun.com>
2020-05-14 18:50:38 +0800

    喜欢

---
+++ src/pages/Workbench/Like/Like.tsx
@@ -0,0 +1,18 @@
+import React, { } from 'react';
+import { Table } from 'antd'
+
+import './Like.scss';
+
+// 收藏
+function Like () {
+
+    return (
+        <div className='app-workbench-like'>
+            
+        </div>
+    )
+
+
+}
+
+export default Like;

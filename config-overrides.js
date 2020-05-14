bda6e04f9f32dde46a31abe707db2d7e8f60f135
张琛琛 <zhangchenchen@qizhuyun.com>
2020-05-14 18:49:31 +0800

    配置主题

--- config-overrides.js
+++ config-overrides.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const { getThemeVariables } = require('antd/dist/theme');
 const { override, fixBabelImports, addWebpackAlias, addLessLoader } = require('customize-cra');
 
 
@@ -18,8 +19,14 @@ module.exports = override(
         ["components"]: path.resolve(__dirname, "src/components"),        
     }),
     // 使用less-loader对源码重的less的变量进行重新制定，设置antd自定义主题
-    // addLessLoader({
-    //     javascriptEnabled: true,
-    //     modifyVars:{'@primary-color':'#1DA57A'},
-    // })
+    addLessLoader({
+        javascriptEnabled: true,
+        modifyVars:{
+            '@primary-color':'#2f4050', // 全局主题
+            '@link-color': '#1DA57A', // 链接色
+            '@border-radius-base': '2px', // 组件/浮层圆角
+            '@font-size-base': '13px', // 主字号
+            'compact': true, // 开启紧凑模式
+        },
+    })
 );
